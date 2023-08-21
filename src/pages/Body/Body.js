632a6dfbf6e332f0af8f036e2464d6ff96b86d32
cengiztuta <tutacengiz@gmail.com>
2023-08-21 22:00:22 +0300

    210823last

--- src/pages/Body/Body.js
+++ src/pages/Body/Body.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Body.css";
 import { ChevronLeftIcon } from "@chakra-ui/icons";
+import CardSlider from "./CardSlider";
 
 const Body = () => {
   return (
@@ -11,8 +12,14 @@ const Body = () => {
     >
       <div className="top-prague-att">
         <a className="top-title">TOP PRAGUE ATTRACTIONS INCLUDED IN COOLPASS</a>
-        <div className="carousel-container"></div>
-       
+        <div className="carousel-container">
+          <div className="card-slide-left-icon"> </div>
+          <div className="card-slide"> </div>
+          <div className="card-slide"> </div>
+          <div className="card-slide"> </div>
+          <div className="card-slide"> </div>
+          <div className="card-slide-right-icon"> </div>
+        </div>
       </div>
     </section>
   );
--- src/pages/Body/CardSlider.js
+++ src/pages/Body/CardSlider.js
@@ -2,22 +2,31 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import "./ImageSlider.css";
+import "./CardSlider.css";
 
-const ImageSlider = () => {
+const CardSlider = () => {
+    const NextArrow = ({ onClick }) => (
+        <div className="slick-arrow slick-next" onClick={onClick}>
+          Next
+        </div>
+      );
+      
+      const PrevArrow = ({ onClick }) => (
+        <div className="slick-arrow slick-prev" onClick={onClick}>
+          Prev
+        </div>
+      );
   const settings = {
-    dots: true,
+    dots: false,
+    arrow: true,
     speed: 1500,
     slidesToShow: 1,
     innerHeight: 500,
     adaptiveHeight: true,
     autoplay: true,
     autoplaySpeed: 6000,
-    customPaging: (i) => (
-      <div className="ft-slick__dots--custom">
-        <div className="loading" />
-      </div>
-    ),
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />
   };
 
   const images = [
@@ -33,7 +42,7 @@ const ImageSlider = () => {
       <Slider {...settings}>
         {images.map((image, index) => (
           <div key={index}>
-            <img className="image" src={image} />
+            <img className="card" src={image} />
           </div>
         ))}
       </Slider>
@@ -41,4 +50,4 @@ const ImageSlider = () => {
   );
 };
 
-export default ImageSlider;
+export default CardSlider;
